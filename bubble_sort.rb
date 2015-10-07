56665998fd0befc73727512a4e3ff5f4fc2156bb
erikvdw <ev176@georgetown.edu>
2015-10-07 21:15:53 +0000

    git init

---
+++ bubble_sort.rb
@@ -0,0 +1,29 @@
+def bubble_sort numbers
+  (numbers.length - 1).times do
+    i = 0 
+    j = 1
+    while j < numbers.length
+      if numbers[i] > numbers[j]
+        numbers.insert(i, numbers.delete_at(j))
+      end
+      i, j = i + 1, j + 1
+    end
+  end
+  numbers
+end
+
+def bubble_sort_by numbers, &how
+  (numbers.length - 1).times do
+    i = 0
+    j = 1
+    while j < numbers.length
+      if (how.call numbers[i], numbers[j]) > 0
+        numbers.insert(i, numbers.delete_at(j))
+      end
+      i, j = i + 1, j + 1
+    end
+  end
+  numbers
+  
+  
+end
